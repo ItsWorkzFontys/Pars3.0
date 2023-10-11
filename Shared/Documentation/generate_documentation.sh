aa60ad8d5d583c6069c254d528328dd59f7d0dfc
RensVanGiersbergen <rensgiersbergen@gmail.com>
2023-10-11 09:33:54 +0200

    png uploaded to gh-pages with workflow, all files in root

--- Shared/Documentation/generate_documentation.sh
+++ Shared/Documentation/generate_documentation.sh
@@ -3,19 +3,19 @@
 cd Shared/Documentation
 
 #Get all .puml files and export those to .png with the same basename
-for file in Diagrams/*.puml; do
+for file in *.puml; do
     filename=$(basename -- "$file")
     filename="${filename%.*}"
     echo -e "Found $filename.puml"
-    puml generate $file -o "Diagrams/$filename.png"
+    puml generate $file -o "$filename.png"
     echo -e "Created $filename.png"
 done
 
 #Get the .adoc files and export them to .html with the same basename
-for file2 in Text/*.adoc; do
+for file2 in *.adoc; do
     filename2=$(basename -- "$file2")
     filename2="${filename2%.*}"
     echo -e "Found $filename2.adoc"
-    asciidoctor "Text/$filename2.adoc"
+    asciidoctor "$filename2.adoc"
     echo -e "Created $filename2.html"
 done
