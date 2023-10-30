56cca409187e0d261911955e4a8e4565d8c817cc
Yarik <Yarik@hetblomhof.nl>
2023-10-30 13:37:26 +0100

    Images

--- Pars_Frontend/pars_validationdashboard/src/Shared/Components/Content/Pages/Dashboard/Dashboard.tsx
+++ Pars_Frontend/pars_validationdashboard/src/Shared/Components/Content/Pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Second from 'src/Shared/Components/Content/Pages/Dashboard/Second.png';
 import 'src/Shared/Components/Content/Pages/Dashboard/dashboard.scss';
 
 function Dashboard(): JSX.Element {
     return(
         <div className='dashboard'>
-            this is the dashboard
+            <h1>Dashboard</h1>
+            <img src= {Second} alt="example"/>
         </div>
     );
 }
