7d9071c5f5bd01f1d7a9b702a64bfbaf60465381
Zandzakje <DamianTomasila@hotmail.com>
2023-11-13 10:37:35 +0100

    Gateway setup

---
+++ Pars_Backend/Gateway/Gateway/Controllers/GatewayController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gateway.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GatewayController
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpGet("hello")]
+        public ActionResult<string> Get2()
+        {
+            return "Hello world";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
