3a3d347d07647d93a7e18b277fffcd1a045a1c54
Zandzakje <DamianTomasila@hotmail.com>
2023-11-20 13:45:51 +0100

    Added two test endpoints

--- Pars_Backend/Pars_UserManagement/Pars_UserManagement/Controllers/UserController.cs
+++ Pars_Backend/Pars_UserManagement/Pars_UserManagement/Controllers/UserController.cs
@@ -2,11 +2,27 @@
 
 namespace Pars_UserManagement.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class UserController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            string number = id * 5;
+            return Ok(number);
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string test = "hello world! :D";
+            return Ok(test);
         }
     }
 }
