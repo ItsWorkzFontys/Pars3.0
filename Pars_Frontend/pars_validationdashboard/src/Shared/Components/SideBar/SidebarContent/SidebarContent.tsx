c82c7c5f8934e2f3f981eddfe596a871e9d7acb5
Sem Plasmeijer <plasmeijer.sem@gmail.com>
2023-11-27 12:03:00 +0100

    everything

--- Pars_Frontend/pars_validationdashboard/src/Shared/Components/SideBar/SidebarContent/SidebarContent.tsx
+++ Pars_Frontend/pars_validationdashboard/src/Shared/Components/SideBar/SidebarContent/SidebarContent.tsx
@@ -8,7 +8,7 @@ function SidebarContent(props:any): JSX.Element {
     return(
         <div>
             <SidebarRow header={false} link="/" rowText="Dashboard" icon={<DashboardIcon inheritViewBox={true} className="icon"/>}/>
-            <SidebarRow header={false} link="/example" rowText="Example" icon={<LiveHelpIcon  className="icon"/>}/>
+            <SidebarRow header={false} link="/example" rowText="Example" icon={<LiveHelpIcon className="icon" />} />
         </div>
     );
 
