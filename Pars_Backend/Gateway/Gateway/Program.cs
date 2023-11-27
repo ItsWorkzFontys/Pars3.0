457255b62c1da0a73e077bbda0daa2514d36ebaa
Zandzakje <DamianTomasila@hotmail.com>
2023-11-27 14:19:11 +0100

    gateway connects successfully with test endpoints

--- Pars_Backend/Gateway/Gateway/Program.cs
+++ Pars_Backend/Gateway/Gateway/Program.cs
@@ -25,6 +25,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseOcelot().Wait();
+
 app.UseAuthorization();
 
 app.MapControllers();
--- Pars_Backend/Pars_UserManagement/Pars_UserManagement/Controllers/UserController.cs
+++ Pars_Backend/Pars_UserManagement/Pars_UserManagement/Controllers/UserController.cs
@@ -2,28 +2,82 @@
 
 namespace Pars_UserManagement.Controllers
 {
-    [Route("user/")]
     [ApiController]
-    public class UserController : Controller
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
     {
         // public IActionResult Index()
         // {
         //     return View();
         // }
 
+        // private readonly IUserValidationService _userValidationService;
+
+        // public UserValidationController(IUserValidationService userValidationService)
+        // {
+        //     _userValidationService = userValidationService;
+        // }
+
+        // GET api/user/idparam/{id}
         [HttpGet]
-        [Route("{id:int}")]
-        public ActionResult<string> Get(int id)
+        [Route("idparam/{id}")]
+        public ActionResult<string> GetGet(int id)
         {
             string number = (id * 5).ToString();
-            return number;
+            return Ok(number);
         }
 
+        // GET api/user/test?id={int}
+        [HttpGet]
+        [Route("test")]
+        public ActionResult<string> Get([FromQuery]int id)
+        {
+            string number = (id * 5).ToString();
+            return Ok(number);
+        }
+
+        // GET api/user
         [HttpGet]
         public ActionResult<string> Get()
         {
             string test = "hello world! :D";
-            return test;
+            return Ok(test);
         }
+
+        // [HttpGet]
+        // [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        // public async Task<IActionResult> GetAll()
+        // {
+        //     return Ok(await _userValidationService.GetUserValidations());
+        // }
+
+        // [HttpGet("{Id:guid}")]
+        // [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        // public async Task<IActionResult> GetById(Guid Id)
+        // {
+        //     return Ok(await _userValidationService.GetValidationById(Id));
+        // }
+
+        // [HttpPost]
+        // [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        // public async Task<IActionResult> AddUser(UserValidationModel userValidation)
+        // {
+        //     return Ok(_userValidationService.AddUserValidation(userValidation));
+        // }
+
+        // [HttpPut]
+        // [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        // public async Task<IActionResult> UpdateUser(UserValidationModel userValidation)
+        // {
+        //     return Ok(_userValidationService.AddUserValidation(userValidation));
+        // }
+
+        // [HttpDelete]
+        // [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        // public async Task<IActionResult> DeleteUserById(Guid Id)
+        // {
+        //     _userValidationService.DeleteUserValidationById(Id);
+        //     return Ok();
+        // }
     }
 }
