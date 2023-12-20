6c0c079fdcb2b916fe09f9f8ab93999bfdde822b
ArtemIvl <74567286+ArtemIvl@users.noreply.github.com>
2023-12-20 11:51:45 +0100

    Configuration services (#78)
    * New service for configuration services
    
    * Initial startup changes
    
    * Controller and database setup for settings
    
    * CRUD operations + 2 databases, admin access, dockerfile
    
    ---------
    
    Co-authored-by: Sem Plasmeijer <68157278+SemPlasmeijer@users.noreply.github.com>
    Co-authored-by: Artemii <456469@student.fontys.nl>

---
+++ Pars_Backend/Pars_ConfigurationServices/Pars_ConfigurationServices/Controllers/ConfigurationController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pars_ConfigurationServices.Models;
+using Pars_ConfigurationServices.Services;
+
+namespace Pars_ConfigurationServices.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ConfigurationController : ControllerBase
+    {
+        private readonly ConfigurationService _configurationService;
+
+        public ConfigurationController(ConfigurationService configurationService)
+        {
+            _configurationService = configurationService;
+        }
+
+        [HttpGet("{schoolId}")]
+        public IActionResult GetSchoolConfiguration(int schoolId)
+        {
+            var configuration = _configurationService.GetSchoolConfiguration(schoolId);
+
+            if (configuration == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(configuration);
+        }
+
+        [HttpPost]
+        public IActionResult CreateSchoolConfiguration([FromBody] SchoolConfiguration schoolConfiguration, int userId)
+        {
+            try
+            {
+                _configurationService.CreateSchoolConfiguration(schoolConfiguration, userId);
+                return Ok("Creation successful");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Creation failed: {ex.Message}");
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteSchoolConfiguration(int userId, int schoolId)
+        {
+            try
+            {
+                _configurationService.DeleteSchoolConfiguration(userId, schoolId);
+                return Ok("Deletion successful");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Deletion failed: {ex.Message}");
+            }
+        }
+
+        [HttpPut("number-of-students")]
+        public IActionResult UpdateNumberOfStudents(int schoolId, int userId, int newNumOfStudents)
+        {
+            var success = _configurationService.UpdateNumberOfStudents(schoolId, userId, newNumOfStudents);
+
+            if (!success)
+            {
+                return Forbid();
+            }
+
+            return Ok();
+        }
+
+        [HttpPut("absence-color")]
+        public IActionResult UpdateAbsenceColor(int schoolId, int userId, string newAbsenceColor)
+        {
+            var success = _configurationService.UpdateAbsenceColor(schoolId, userId, newAbsenceColor);
+
+            if (!success)
+            {
+                return Forbid();
+            }
+
+            return Ok();
+        }
+
+        [HttpPut("presence-color")]
+        public IActionResult UpdatePresenceColor(int schoolId, int userId, string newPresenceColor)
+        {
+            var success = _configurationService.UpdatePresenceColor(schoolId, userId, newPresenceColor);
+
+            if (!success)
+            {
+                return Forbid();
+            }
+
+            return Ok();
+        }
+
+        [HttpPut("late-color")]
+        public IActionResult UpdateLateColor(int schoolId, int userId, string newLateColor)
+        {
+            var success = _configurationService.UpdateLateColor(schoolId, userId, newLateColor);
+
+            if (!success)
+            {
+                return Forbid();
+            }
+
+            return Ok();
+        }
+
+
+        [HttpPut("{schoolId}/{userId}")]
+        public ActionResult UpdateSchoolConfiguration(int schoolId, int userId, [FromBody] SchoolConfiguration newConfiguration)
+        {
+            var success = _configurationService.UpdateSchoolConfiguration(schoolId, newConfiguration, userId);
+
+            if (!success)
+            {
+                return Forbid(); // or BadRequest(), depending on your error handling strategy
+            }
+
+            return Ok();
+        }
+    }
+}
+
---
+++ Pars_Backend/Pars_ConfigurationServices/Pars_ConfigurationServices/Services/ConfigurationService.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using Pars_ConfigurationServices.Data;
+using Pars_ConfigurationServices.Models;
+
+namespace Pars_ConfigurationServices.Services
+{
+	public class ConfigurationService
+	{
+        private readonly ApplicationDbContext _context;
+
+        public ConfigurationService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public SchoolConfiguration GetSchoolConfiguration(int schoolId)
+        {
+            var school = _context.SchoolConfigurations.FirstOrDefault(c => c.schoolId == schoolId);
+
+            if (school != null)
+            {
+                var schoolData = new SchoolConfiguration
+                {
+                    id = school.id,
+                    schoolId = school.schoolId,
+                    numberOfStudents = school.numberOfStudents,
+                    absenceColor = school.absenceColor,
+                    presenceColor = school.presenceColor,
+                    lateColor = school.lateColor
+                };
+
+                return schoolData;
+            }
+            return null;
+        }
+
+        public void CreateSchoolConfiguration(SchoolConfiguration schoolConfiguration, int userId)
+        {
+            var isAdmin = _context.AdminPermissions.Any(ap => ap.userId == userId && ap.isAdmin);
+            if (!isAdmin)
+            {
+                throw new Exception("You can't create new school configuration");
+            }
+
+            if (schoolConfiguration != null)
+            {
+                var schoolData = new SchoolConfiguration
+                {
+                    schoolId = schoolConfiguration.schoolId,
+                    numberOfStudents = schoolConfiguration.numberOfStudents,
+                    absenceColor = schoolConfiguration.absenceColor,
+                    presenceColor = schoolConfiguration.presenceColor,
+                    lateColor = schoolConfiguration.lateColor
+                };
+
+                _context.Add(schoolData);
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeleteSchoolConfiguration(int userId, int schoolId)
+        {
+            var isAdmin = _context.AdminPermissions.Any(ap => ap.userId == userId && ap.isAdmin);
+            if (!isAdmin)
+            {
+                throw new Exception("You can't delete school configuration");
+            }
+
+            var school = _context.SchoolConfigurations.FirstOrDefault(c => c.schoolId == schoolId);
+
+            if (school != null)
+            {
+                _context.Remove(school);
+                _context.SaveChanges();
+            }
+        }
+
+        public bool UpdateNumberOfStudents(int schoolId, int userId, int newNumOfStudents)
+        {
+            var isAdmin = _context.AdminPermissions.Any(ap => ap.userId == userId && ap.isAdmin);
+            if (!isAdmin)
+            {
+                return false;
+            }
+
+            var targetSchool = _context.SchoolConfigurations.FirstOrDefault(s => s.schoolId == schoolId);
+
+            if (targetSchool == null)
+            {
+                return false;
+            }
+
+            targetSchool.numberOfStudents = newNumOfStudents;
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool UpdateAbsenceColor(int schoolId, int userId, string newAbsenceColor)
+        {
+            var isAdmin = _context.AdminPermissions.Any(ap => ap.userId == userId && ap.isAdmin);
+            if (!isAdmin)
+            {
+                return false;
+            }
+
+            var targetSchool = _context.SchoolConfigurations.FirstOrDefault(s => s.schoolId == schoolId);
+
+            if (targetSchool == null)
+            {
+                return false;
+            }
+
+            targetSchool.absenceColor = newAbsenceColor;
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool UpdatePresenceColor(int schoolId, int userId, string newPresenceColor)
+        {
+            var isAdmin = _context.AdminPermissions.Any(ap => ap.userId == userId && ap.isAdmin);
+            if (!isAdmin)
+            {
+                return false;
+            }
+
+            var targetSchool = _context.SchoolConfigurations.FirstOrDefault(s => s.schoolId == schoolId);
+
+            if (targetSchool == null)
+            {
+                return false;
+            }
+
+            targetSchool.presenceColor = newPresenceColor;
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool UpdateLateColor(int schoolId, int userId, string newLateColor)
+        {
+            var isAdmin = _context.AdminPermissions.Any(ap => ap.userId == userId && ap.isAdmin);
+            if (!isAdmin)
+            {
+                return false;
+            }
+
+            var targetSchool = _context.SchoolConfigurations.FirstOrDefault(s => s.schoolId == schoolId);
+
+            if (targetSchool == null)
+            {
+                return false;
+            }
+
+            targetSchool.lateColor = newLateColor;
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool UpdateSchoolConfiguration(int schoolId, SchoolConfiguration newConfiguration, int userId)
+        {
+            var isAdmin = _context.AdminPermissions.Any(ap => ap.userId == userId && ap.isAdmin);
+            if (!isAdmin)
+            {
+                return false;
+            }
+
+            var existingConfiguration = _context.SchoolConfigurations
+                .FirstOrDefault(c => c.schoolId == schoolId);
+
+            if (existingConfiguration == null)
+            {
+                return false;
+            }
+
+            existingConfiguration.numberOfStudents = newConfiguration.numberOfStudents;
+            existingConfiguration.presenceColor = newConfiguration.presenceColor;
+            existingConfiguration.absenceColor = newConfiguration.absenceColor;
+            existingConfiguration.lateColor = newConfiguration.lateColor;
+
+            _context.SaveChanges();
+            return true;
+        }
+    }
+}
+
